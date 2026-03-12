b3c20fdca021ae7d33e88354fd312b921f6573f1
Matias Catala <matiasncatala@gmail.com>
2026-03-12 18:51:16 -0300

    adding .env & mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  //Importar librerias de terceros primero
+  imports: [
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+  ],
   controllers: [],
   providers: [],
 })
